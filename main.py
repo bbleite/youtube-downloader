442eb0f4ccaff1816daaf75371683b1bebd92d86
Bernardo Leite <bbleite85@gmail.com>
2023-08-22 16:27:25 -0500

    download function misc

--- main.py
+++ main.py
@@ -11,5 +11,12 @@ app = customtkinter.CTk() # Initialize custom tkinter object
 app.geometry("720x480")
 app.title("YouTube Downloader")
 
+# Add UI Elements
+title = customtkinter.CTkLabel(app, text="Insert a YouTube link") # Title label
+title.pack(padx=10, pady=10) # Padding
+
+# Link input
+link = customtkinter.CTkEntry(app,  width=350, height=40)
+link.pack()
 # Run app
 app.mainloop()
